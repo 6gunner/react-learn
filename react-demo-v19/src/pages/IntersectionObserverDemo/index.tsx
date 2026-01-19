7613c74ffbf052f4ec1274c0c6058a718644d374
coda <ky302511526@gmail.com>
2026-01-19 20:25:59 +0800

    增加累计时间

--- react-demo-v19/src/pages/IntersectionObserverDemo/index.tsx
+++ react-demo-v19/src/pages/IntersectionObserverDemo/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from "react";
 import { useStyles } from "./style";
-
+import { produce } from "immer";
 interface AdData {
   bgcolor: string;
   title: string;
@@ -8,8 +8,10 @@ interface AdData {
 }
 
 interface AdDataWithTimer extends AdData {
-  startTime?: number;
-  totalViewTime?: number;
+  visible?: boolean;
+  startTime?: number; // 最后一次开始可见的时间戳
+  lastViewTime?: number; // 上次可见时长
+  totalViewTime?: number; // 累计可见时长
   totalViewTimeText?: string;
 }
 
@@ -39,21 +41,11 @@ const ALL_DATAS: AdData[] = [
 export default function IntersectionObserverDemo() {
   const { styles } = useStyles();
 
-  const [ads, setAds] = React.useState<AdData[]>([]);
+  const [ads, setAds] = React.useState<AdData[]>(ALL_DATAS.slice(0, 3));
   const visibleAds = React.useRef<Map<string, AdDataWithTimer>>(new Map());
 
   // 触发re-render
   const [, setTick] = React.useState<number>(0);
-
-  // 初始化ad数据
-  useEffect(() => {
-    const tempAds = [];
-    for (let i = 0; i < 3; i++) {
-      const ad = ALL_DATAS[Math.floor(Math.random() * ALL_DATAS.length)];
-      tempAds.push(ad);
-    }
-    setAds(tempAds);
-  }, []);
 
   const contentBoxRef = React.useRef<HTMLElement>(null);
 
@@ -74,15 +66,21 @@ export default function IntersectionObserverDemo() {
         const adBox = entry.target as HTMLElement;
         const adId = adBox.getAttribute("data-ad-id");
         if (!adId) return;
-        const visbleAdData = visibleAds.current.get(adId);
         const adData = ads[Number(adId)];
+        const visbleAdData = visibleAds.current.get(adId) || { ...adData };
         if (entry.isIntersecting) {
           if (entry.intersectionRatio >= 0.75) {
-            if (visbleAdData) return; // 已经在可见列表中
             const startTime = entry.time;
-            visibleAds.current.set(adId, { ...adData, startTime });
+            visibleAds.current.set(adId, { ...visbleAdData, startTime, visible: true });
           }
         } else {
+          visibleAds.current.set(adId, {
+            ...visbleAdData,
+            visible: false,
+            startTime: 0,
+            lastViewTime: visbleAdData?.totalViewTime || 0,
+          });
+          console.log("ad not visible", adId, "lastViewTime", visbleAdData?.totalViewTime);
           if (entry.intersectionRatio === 0.0 && Number(visbleAdData?.totalViewTime) >= 30000) {
             replaceAd(adId);
           }
@@ -143,16 +141,17 @@ export default function IntersectionObserverDemo() {
     // Map 需要使用 forEach 或 entries() 来遍历，不能用 Object.entries()
     visibleAds.current.forEach(adData => {
       const startTime = adData.startTime || 0;
-      if (startTime) {
+      debugger;
+      if (adData.visible) {
         const elapsed = currentTime - startTime;
-        const minutes = Math.floor(elapsed / 60000);
-        const seconds = Math.floor((elapsed % 60000) / 1000);
+        const totalViewTime = (adData.lastViewTime || 0) + elapsed;
+        const minutes = Math.floor(totalViewTime / 60000);
+        const seconds = Math.floor((totalViewTime % 60000) / 1000);
         const timeText = `${minutes}:${seconds.toString().padStart(2, "0")}`;
         adData.totalViewTimeText = timeText;
-        adData.totalViewTime = elapsed;
+        adData.totalViewTime = totalViewTime;
       }
     });
-
     setTick(tick => tick + 1);
   }, []);
 
@@ -160,7 +159,6 @@ export default function IntersectionObserverDemo() {
     const intervalId = setInterval(() => {
       updateTime();
     }, 1000);
-
     return () => {
       clearInterval(intervalId);
     };
